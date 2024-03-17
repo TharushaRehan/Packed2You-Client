c9369519174276c096e16ec7c750fa25cbf51fbc
Tharusha Perera <tharushaperera088@gmail.com>
2024-03-17 21:43:56 +0530

    update the docker file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [process.env.NEXT_PUBLIC_SUPABASE_DOMAIN],
+    remotePatterns: [
+      {
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_DOMAIN,
+        port: "",
+      },
+    ],
     dangerouslyAllowSVG: true,
   },
   output: "standalone",
--- src/components/global/category.tsx
+++ src/components/global/category.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 interface Props {
   name: string;
   image: any;
 }
 const Category: React.FC<Props> = ({ name, image }) => {
+  const supabase = createClientComponentClient();
+  const [categoryImages, setCategoryImages] = useState<any>();
+
+  // useEffect(() => {
+  //   getImageURLs();
+  // }, []);
+  // const getImageURLs = async () => {
+  //   try {
+  //     const { data } = supabase.storage
+  //       .from("category-images") // Replace with your bucket name
+  //       .getPublicUrl("categoryImage.62ab2c05-57f9-430a-9291-fa7af2b9a5a3");
+
+  //     if (!data) {
+  //       //console.error("error getting public url for", iconId);
+  //       return null; // Or handle the error differently
+  //     }
+  //     console.log(data?.publicUrl);
+  //     setCategoryImages(data?.publicUrl);
+  //   } catch (error) {
+  //     console.log("Error downloading image: ", error);
+  //   }
+  // };
+
   return (
     <div className="border border-neutral-200 rounded-lg py-3 space-y-2 w-[200px] flex flex-col items-center justify-center">
-      <Image src={image} alt={name} width={130} />
+      <Image src={image} alt={name} width={130} height={130} />
       <p className="text-base text-black/80">{name}</p>
     </div>
   );
