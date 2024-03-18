5f37358b2f705d1358365580be71fddbf39a7498
Tharusha Perera <tharushaperera088@gmail.com>
2024-03-18 11:08:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
-import { hostname } from "os";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         hostname: process.env.NEXT_PUBLIC_SUPABASE_DOMAIN,
-        port: "",
       },
     ],
     dangerouslyAllowSVG: true,
